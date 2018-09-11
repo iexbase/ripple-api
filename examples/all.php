07c938286fe6e85d3cc4ee768e781792837bb528
OwlChange <steein.shamsudin@gmail.com>
2018-09-11 07:29:56 +0300

    Added folder with examples

--- examples/all.php
+++ examples/all.php
@@ -1,8 +1,8 @@
 <?php
 include_once '../vendor/autoload.php';
 
-$address = "rBgGyvJSPbF8DS1ZW28nZAMFQJgvGVvxs6";
-$tx = "2711AFC4B3C14B89176954427018127C58253516522CDD91A7B2ADD6E9081EB5";
+$address = "";
+$tx = "";
 $ripple = new \IEXBase\RippleAPI\Ripple($address);
 
 //История транзакций
--- examples/find-transaction.php
+++ examples/find-transaction.php
@@ -1,8 +1,8 @@
 <?php
 include_once '../vendor/autoload.php';
 
-$address = "rBgGyvJSPbF8DS1ZW28nZAMFQJgvGVvxs6";
-$tx = "81DA6BB94ECB56B4798115B14D3931A333FE97703CF50EA87439E353BFCC8C10";
+$address = "";
+$tx = "";
 $ripple = new \IEXBase\RippleAPI\Ripple($address);
 
 // Получаем детали транзакции
